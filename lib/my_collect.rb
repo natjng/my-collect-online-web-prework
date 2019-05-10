bf7df90b51622f91b8347864dc538d59e05aca60
Natalie J <natsetc@gmail.com>
2019-05-10 09:42:56 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0
   new_collection = []
   while i < collection.length
-    new_collection << yield(collection[i] {|x| x.split(" ").first})
+    new_collection << yield(collection[i] {|x| x.split(" ").first}) #or just new_collection << yield(collection[i])
     i += 1
   end
   new_collection
